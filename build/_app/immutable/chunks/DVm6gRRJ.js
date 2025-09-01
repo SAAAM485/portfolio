3dd17d23da205d50e7ab45360b18c2ed18a4cac1
SAAAM485 <143632382+SAAAM485@users.noreply.github.com>
2025-09-01 16:18:45 +0800

    deploy: build for github pages

---
+++ build/_app/immutable/chunks/DVm6gRRJ.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_q4o8hk?.base??"/portfolio",o=globalThis.__sveltekit_q4o8hk?.assets??s;export{o as a,s as b};
--- build/_app/immutable/chunks/p2vmPxBT.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_js0vyr?.base??"/portfolio",t=globalThis.__sveltekit_js0vyr?.assets??s;export{t as a,s as b};
